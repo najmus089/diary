e8a677a542429299fe4a049187a2df2d5c2d5193
najmus089 <136845024+najmus089@users.noreply.github.com>
2023-06-22 15:20:16 +0600

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,6 +1,9 @@
-struct DiaryEntry {
-    int day;
-    int month;
-    int year;
-    char entry[1000];
+
+
+struct diaryEntry{
+int day;
+int month;
+int year;
+char entry[1000];
 };
+typedef struct diaryEntry diary;
