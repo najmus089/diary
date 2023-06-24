b0864af8898ddf43ca77ca43c1d009069b876e91
najmus089 <136845024+najmus089@users.noreply.github.com>
2023-06-24 23:23:05 +0600

    Update addEntry.h

--- addEntry.h
+++ addEntry.h
@@ -1,16 +1,24 @@
 
-void addEntry(diary entries[],int *count){
-diary newentry;
-printf("\nEnter the date (DD MM YYYY): ");
-if(scanf("%d%*c%d%*c%d",&newentry.day,&newentry.month,&newentry.year)!=3){
+void addEntry(diary entries[], int* count) {
+    diary newentry;
+    printf("\nEnter the date (DD MM YYYY): ");
+    if (scanf("%d%*c%d%*c%d", &newentry.day, &newentry.month, &newentry.year) != 3) {
         printf("\nInvalid date format. Entry not added.\n");
-        while(getchar()!='\n');
+        while (getchar() != '\n');
         return;
+    }
+
+    printf("Enter the entry (press Ctrl+Z on a new line to finish):\n");
+   // getchar();
+    int i = 0;
+    char c;
+    while ((c = getchar()) != EOF && c != '\x1A' && i < sizeof(newentry.entry) - 1) {
+        newentry.entry[i++] = c;
+    }
+    newentry.entry[i] = '\0';
+
+    entries[*count] = newentry;
+    (*count)++;
+    printf("\nEntry added successfully!\n");
 }
-printf("Enter the entry:\n ");
-getchar();
-fgets(newentry.entry,sizeof(newentry.entry),stdin);
-entries[*count]=newentry;
-(*count)++;
-printf("\nEntry added successfully!\n");
-}
+
